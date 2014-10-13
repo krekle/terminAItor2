128853a34bf1b0a0855d44cc69053bdeffc384b7
Thomas Gautvedt <thomasgautv@hotmail.com>
2014-10-13 14:47:03 +0200

    Some comments

--- lets try again/board.py
+++ lets try again/board.py
@@ -23,62 +23,105 @@ class Board:
         self.y = y
         self.k = k
         
-        # Populate the board with empty nodes
+        # Populate the board
         self.board = [[node.Node(self) for i in range(x)] for j in range(y)]
         self.populate()
     
+    #
+    # Get node at a given position
+    #
+    
     def get_node(self, node_x, node_y):
+        # Check if valid position
         if node_x >= 0 and node_x < self.x and node_y >= 0 and node_y < self.y:
+            # Valid position, return node
             return self.board[node_y][node_x]
         else:
+            # Not valid, return false
             return None
     
+    #
+    # Move a active node up or down
+    #
+    
     def move(self, pos_x, pos_y, direction):
+        # Reset old node
         old_node = self.get_node(pos_x, pos_y)
+        old_node.value = None
         
-        if old_node is not None:
-            old_node.value = None
-            
+        # Check what way to move
         if direction is 'up':
             pos_y += 1
         else:
             pos_y -= 1
         
+        # Get new node
         new_node = self.get_node(pos_x, pos_y)
-        
-        if new_node is not None:
-            new_node.value = True
+        new_node.value = True
+    
+    #
+    # Calulcate score
+    #
     
     def get_score(self):
         return 5
     
+    #
+    # Populate the board
+    #
     
     def populate(self):
+        # Loop each column
         for pos_x in range(self.x):
+            # Score how many we have generate for this column
             generated = 0
+            
+            # While true because we might generate nodes that are placed over other
             while True:
+                # Check if we have generated all the nodes we need
                 if generated == self.k:
+                    # We have, break
                     break
                 
+                # Generate a random y position
                 pos_y = random.randint(0, self.y - 1)
+                
+                # Get node at this position
                 temp_node = self.get_node(pos_x, pos_y)
                 
+                # Check current value
                 if temp_node.value is None:
+                    # Not already active, activate
                     temp_node.value = True
+                    
+                    # Increase number of generated nodes
                     generated += 1
     
+    #
+    # Returns the first node we can find
+    #
+    
     def get_start_node(self):
+        # Loop x
         for pos_x in range(self.x):
+            # Loop y
             for pos_y in range(self.y):
+                # Get node at current position
                 temp_node = self.get_node(pos_x, pos_y)
+                
+                # Check the value
                 if temp_node.value is not None:
+                    # Node is active, return this
                     return temp_node
+        
+        # This should never happen
         return None
     
+    #
+    # Debug
+    #
     
     def print_pretty(self):
         print "---------------"
         for i in range(self.y):
             print self.board[i]
-        
-    
--- lets try again/node.py
+++ lets try again/node.py
@@ -1,65 +1,112 @@
+
+#
+# Imports
+#
+
 import copy
 
+'''
+Node
+One Node on the board
+'''
+
 class Node:
+    
+    #
+    # Constructor
+    #
+    
     def __init__(self, board):
         self.board = board
         self.value = None
+    
+    #
+    # toString()
+    #
     
     def __repr__(self):
         if self.value is None:
             return ' '
         else:
             return 'o'
-        
-        #x, y = self.get_pos()
-        
-        #if x is not None:
-        #    return '[' + str(x) + ',' + str(y) + ']'
-        #else:
-        #    return 'Err'
+    
+    #
+    # Returns the position for this node (in a hackyyy way)
+    #
     
     def get_pos(self):
-        for i in range(self.board.y):
-            for j in range(self.board.x):
-                if self.board.get_node(j, i) == self:
-                    return j, i
+        # Loop y
+        for y in range(self.board.y):
+            # Loop x
+            for x in range(self.board.x):
+                # Check if current node is self
+                if self.board.get_node(y, x) == self:
+                    # This is the correct node, return pos
+                    return y, x
         
+        # This node has no positions? WTF
         return None, None
     
+    #
+    # Check if move up/down is valid for this
+    #
+    
     def is_valid_position(self, direction):
+        # Get current position
         pos_x, pos_y = self.get_pos()
         
+        # Check what direction to move
         if direction is 'up':
             pos_y += 1
         else:
             pos_y -= 1
         
+        # Check if desiered move is valid
         if pos_x >= 0 and pos_x < self.board.x and pos_y >= 0 and pos_y < self.board.y and self.board.get_node(pos_x, pos_y).value is None:
+            # Valid, yolo
             return True
         else:
+            # Not valid
             return False
     
+    #
+    # Generate a life of neighbours for this node
+    #
+    
     def get_neighbours(self):
+        # Store in this array
         neighbours = []
         
+        # Loop y
         for y in range(self.board.y):
+            # Loop x
             for x in range(self.board.x):
+                # Get node at current position
                 temp_node = self.board.get_node(x, y)
+                
+                # Only keep going if the current node is active
                 if temp_node is not None:
+                    # Active, check up move
                     if temp_node.is_valid_position('up') is True:
+                        # Up move is valid, add to array
                         temp_board1 = copy.deepcopy(self.board)
                         temp_board1.move(x, y, 'up')
                         neighbours.append(temp_board1)
                     
+                    # Check down move
                     if temp_node.is_valid_position('down') is True:
+                        # Down move is valid, add to array
                         temp_board2 = copy.deepcopy(self.board)
                         temp_board2.move(x, y, 'down')
                         neighbours.append(temp_board2)
-                    break
         
+        # Return list
         return neighbours
     
+    #
+    # Return the score
+    #
+    
     def get_score(self):
-        cost = 0.5
-        
-        return cost
+        # We just call the method implemented in board
+        return self.board.get_score()
